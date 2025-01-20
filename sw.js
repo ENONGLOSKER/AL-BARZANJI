403c91d09a57d1669a2e8f15148b174e8fb0ea31
ENONGLOSKER <ENONGLOSKER@gmail.com>
2025-01-20 07:30:46 +0700

    add: sw.js

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'offline-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/details.html',
+    '/tentang.html',
+    '/script.js',
+    '/img/bg-head.svg',
+    '/img/bg.svg',
+    '/img/bismi.png',
+    '/img/icon-menu.png',
+    '/img/icon-min.png',
+    '/img/icon-panah.svg',
+    '/img/icon-pause.png',
+    '/img/icon-play.png',
+    '/img/icon-plus.png',
+    '/img/iconsayat.png',
+    '/img/logobz.png',
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Fetch request
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        )
+    );
+});
